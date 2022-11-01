7f3c53a4e1b84f967d405313f1b0def4761b03b0
Taisia Ivanovskaya <taya.naumova@gmail.com>
2022-11-01 17:49:08 +0300

    TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,28 @@
 package ru.netology.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
 
-class CashbackHackServiceTest {
+import static org.testng.Assert.*;
 
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculateAmount() {
+        CashbackHackService amount = new CashbackHackService();
+        int purchase = 1500;
+        int expected = 500;
+        int actual = amount.remain(purchase);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldNotCalculateAmountWhen1000() {
+        CashbackHackService amount = new CashbackHackService();
+        int purchase = 1000;
+        int expected = 0;
+        int actual = amount.remain(purchase);
+        assertEquals(actual, expected);
+
+
+    }
 }
